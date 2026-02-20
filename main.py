08227252d965b1dfecdfbe302e588a21a212797c
Mateusz Kuniczuk <mateuszkuniczuk98@gmail.com>
2026-02-20 23:21:52 +0100

    lil fixy

--- main.py
+++ main.py
@@ -14,16 +14,9 @@ def main(path_to_consolidated_statement: str) -> None:
     Args:
         path_to_consolidated_statement: path to your CSV report generated in Revolut
     """
-    val = convert(
-        amount=100.0,
-        currency=Currency.EUR,
-        conversion_date=datetime.date(year=2025, month=1, day=13),
-    )
-    print(val)
-
-    # report = ConsolidatedReportManager('data/consolidated-statement_2025-01-01_2025-12-31_pl-pl_a1aa72.csv')
     report = ConsolidatedReportManager(path_to_consolidated_statement)
-    print(report)
+
+    print("Goodbye!")
 
 
 if __name__ == "__main__":
